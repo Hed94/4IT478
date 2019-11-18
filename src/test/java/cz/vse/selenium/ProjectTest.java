41e2e6c4b51967cee6f6fddf9d9e50a4d66edf64
Miroslav Mareš <Hed94@seznam.cz>
2019-11-18 18:51:07 +0100

    Update ProjectTest.java

--- src/test/java/cz/vse/selenium/ProjectTest.java
+++ src/test/java/cz/vse/selenium/ProjectTest.java
@@ -120,9 +120,9 @@ public class ProjectTest {
         Assert.assertTrue(radek != null);
 
         // Smazání a kontrola že už tam neni
-        wait = new WebDriverWait(driver, 2);
-        wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("uniform-delete_confirm")));
-        driver.findElement(By.id("delete_confirm")).click();
+       // wait = new WebDriverWait(driver, 2);
+        //wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("uniform-delete_confirm")));
+        //driver.findElement(By.id("delete_confirm")).click();
         driver.findElement(By.className("btn-primary-modal-action")).click();
         elements = driver.findElements(By.cssSelector("[class='table table-striped table-bordered table-hover'] tr"));
         Assert.assertTrue(!elements.contains(radek));
